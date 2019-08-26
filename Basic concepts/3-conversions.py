b26e2b130553e0f5431de7f60f3fa740bc4587b4
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-08-26 06:46:11 -0700

    Data type conversions

---
+++ Basic concepts/3-conversions.py
@@ -0,0 +1,16 @@
+# In order to convert string into int, float or convert int, float we use this
+
+# Convert string into int
+num= int(input('enter number'))
+print('number in int = ',num)
+
+# Convert string into float
+num1= float(input('enter number'))
+print('number in float = ',num1)
+
+# Convert int into string
+a = str(92)
+print('Number in string = ',a)
+
+# Number in string means we cannot applay mathematical operations on it
+

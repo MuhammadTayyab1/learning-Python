966875b5ff639455b16ddf7a2c727ea9f291794b
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-08-26 06:47:00 -0700

    conditions (if, else)

---
+++ Basic concepts/4-conditions.py
@@ -0,0 +1,8 @@
+value = int(input("Enter value"))
+
+if value>100:
+    print('Value is greator then 100')
+else:
+    print('Value is less then 100')
+    
+# You can either use single quotation or double quotation

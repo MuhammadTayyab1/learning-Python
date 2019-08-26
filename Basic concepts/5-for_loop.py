462531bbc054900ff0f7a792bf66f9eb38f1734b
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-08-26 10:12:42 -0700

    For loop

---
+++ Basic concepts/5-for_loop.py
@@ -0,0 +1,30 @@
+# for loop range 1 to 10
+for i in range(10):
+    print(i)
+   
+
+
+# for loop start from index 2
+for x in range(2, 6):
+  print(x)
+
+
+
+# for with increament of more then 1
+for y in range(1,20,3):
+    print(y)
+
+
+
+# Nested for loop
+for a in range(2):
+    for b in range(2):
+        print(a,"  ",b)
+
+
+
+# else in for loop
+for j in range(4):
+    print(j)
+else:
+    print('Program end')

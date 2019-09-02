29cf6d5c7387c178edc23da63d8441063e394869
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-09-02 13:27:11 -0700

    Search index with if condition

---
+++ Basic concepts/12-search_with_if_condition.py
@@ -0,0 +1,17 @@
+# search from string index
+name= "someone"
+
+if "m" in name:
+    print("find")
+
+
+
+
+# Search in cause of not found
+name= "someone"
+
+if "r" not in name:
+    print("not found")
+
+
+

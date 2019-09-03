1d3b99af97e5f75fd9d7adc7501406cfe70ef466
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-09-03 08:10:41 -0700

    Array using numpy

--- Basic concepts/11-Array_lib.py
+++ Basic concepts/11-Array_lib.py
@@ -5,3 +5,29 @@ import numpy as np
 arr=np.array([1,2,3])
 
 print(arr/2)
+
+
+
+# array through loop
+arr= np.array([10,20,30,40])
+
+for i in range(len(arr)):
+    print(arr[i])
+    
+    
+    
+# Conversion 
+
+import numpy as nm
+
+a=[90,98,78,87]
+
+# list to array
+b=nm.array(a)
+
+print(b/2)
+
+# array to list
+q=list(b)
+
+print(q/2)

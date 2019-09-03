07e464504afe6d5f3e464c63fb332db72039d3e1
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-09-03 09:01:45 -0700

    Math

---
+++ Basic concepts/14-math_fnc.py
@@ -0,0 +1,36 @@
+import math
+
+a=5
+# Square
+print(a**2)
+
+# Factorial
+fac=math.factorial(a)
+print(fac)
+
+# remainder
+rem= math.remainder(a,4)
+print(rem)
+
+# cos 
+cos= math.cos(a)
+print(cos)
+
+# sin
+sin= math.sin(a)
+print(sin)
+
+# tan
+tan= math.tan(a)
+print(tan)
+
+# degree
+deg= math.degrees(sin)
+print(deg)
+
+# radian
+rad= math.radians(deg)
+print(rad)
+
+# round off values
+print(math.ceil(3.9))

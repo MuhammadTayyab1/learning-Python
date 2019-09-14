6737629b5049a913e9677b195717d5db48212f37
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-09-14 13:54:57 -0700

    Doctors clinic time detector

---
+++ small programs/Doctors_Clinic_time_cal.py
@@ -0,0 +1,6 @@
+cs=int(input(""))
+
+for i in range(cs):
+    data=input("Enter number of patient and time seperated by space").split()
+    total= (10 - int(data[1]))*(int(data[0])-1)
+    print(total)

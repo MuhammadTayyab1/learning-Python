a5e01497af9e73e1d58aa8750d2b8ca8ad6aaf95
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-09-15 13:28:25 -0700

    Ship

---
+++ Patterns_of_star/ship.py
@@ -0,0 +1,16 @@
+for i in range(8):
+    for j in range(40-i):
+        print(" ",end="")
+    for k in range(i):
+        print("* ",end="")
+    print("")
+    
+    
+for i in range(10):
+    for k in reversed(range(10+i)):
+        print(" ",end="")
+    for j in range(30-i):
+        print("*",end="")
+    for m in range(30-i):
+        print("*",end="")
+    print("")

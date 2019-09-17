d424ddf41cb6e05282a31b635e15f36fe2e7e013
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-09-17 06:50:54 -0700

    Text to Pdf

--- Using Libraries/text_to_pdf.py
+++ Using Libraries/text_to_pdf.py
@@ -2,9 +2,15 @@ from fpdf import FPDF
 
 pdf=FPDF()
 pdf.add_page()
+
+# Font size
 pdf.set_font("Arial",size=12)
+
+# line 1, align center
 pdf.cell(200,10,txt="hello",ln=1,align="C")
-pdf.cell(200,10,txt="hello",ln=2,align="C")
+
+# line 2, align center
+pdf.cell(200,10,txt="This is my data",ln=2,align="C")
 
 #  path of file
-pdf.output("C:\\Users\\Tayyab\\Desktop\\I.T E-Books\\iot.pdf")
+pdf.output("C:\\Users\\Tayyab\\Desktop\\name.pdf")

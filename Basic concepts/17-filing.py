60921124942f1503bc5490fbb92725fb634926f6
Muhammad Tayyab <43279438+MuhammadTayyab1@users.noreply.github.com>
2019-10-07 01:03:36 -0700

    filing data write and read

---
+++ Basic concepts/17-filing.py
@@ -0,0 +1,12 @@
+
+# r read
+# w write
+# a+ append
+
+fs= open(r'C:\Users\Tayyab\Desktop\newfile.txt','w')
+fs.write("This is my data")
+fs.close()
+
+fs1= open(r'C:\Users\Tayyab\Desktop\newfile.txt','r')
+hold=fs1.read()
+print(hold)
